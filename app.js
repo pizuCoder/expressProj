9b6682450c139f5cadafbe60de564004a533ddef
Priya Moghe <102720207+pizuCoder@users.noreply.github.com>
2023-05-11 13:12:23 +0530

    routers and filters

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 // const http = require("http");
 const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
-app.use((req, res, next) => {
-  console.log("In the middleware!");
-  
-  next();
-});
-app.use((req, res, next) => {
-  console.log("In another middleware!");
-  res.send('<h1> hello to node js </h1>')
-});
-app.listen(3000)
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"> <select name="size"> <option value="L"> L </option> <option value="M"> M </option> <option value="S"> S </option> </select> <button type=submit>Add</button></form>')
+    });
+    app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    
+    res.redirect('/');
+    });
+    app.use('/', (req, res, next) => {
+    res.send('<h1>Hello from Express!</h1>');
+    });
+app.listen(3000);
